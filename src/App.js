22dbb40762d3fe06d1a908ca6d48dc8792a0027a
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-12-07 11:06:14 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-
-import Home from './Home'
-import Movie from './SingleMovie'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Movie from "./SingleMovie";
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/movies/:id'>
+          <Movie />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
